131a335fa0492717a2f2ffbbe1ceb422e670c1ae
brendancaywood <brendan.m.caywood@gmail.com>
2018-11-14 16:43:52 -0500

    Login code

--- login.js
+++ login.js
@@ -9,3 +9,22 @@ function create(){
     // ...
 });
 }
+
+function login(){
+    var email = document.getElementById("emailin").value
+    console.log(email)
+    var password = document.getElementById("passwordin").value;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log("worked")
+    } else {
+      console.log("didn't work")
+    }
+  });
